3d5a412b97d6bd01ace93d56aed661e1647bebc3
sndstudy <sanada2054tech@gmail.com>
2018-07-30 01:07:00 +0900

    モジュール化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Sub1Module } from './sub1_module/sub1.module';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Sub1Module
   ],
   providers: [],
   bootstrap: [AppComponent]
